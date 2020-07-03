2f36ca3768cde19b3d42de14f99f524afd08ec50
tshardul <shardult16@gmail.com>
2020-07-03 20:08:21 +0530

    update radio stations

--- station.js
+++ station.js
@@ -19,6 +19,7 @@ $('.Play').on('click',function(e){
     $('.intro').addClass('hidden')
     $('.select').removeClass('hidden')
     $('.genre-button').removeClass('hidden')
+    $('button').removeClass('active')
 })
 
 $('.title-name').on('click', function(e){
@@ -60,11 +61,11 @@ music.on('click', function(e){
 })
 
 $('#shuffleButton').on('click', function(e){
-    var which = Math.floor(Math.random()*8);
+    var which = Math.floor(Math.random()*24);
     if (played[-1] === which){
-        var which = Math.floor(Math.random()*8);
+        var which = Math.floor(Math.random()*24);
         if (played[-1] === which){
-            var which = Math.floor(Math.random()*8);
+            var which = Math.floor(Math.random()*24);
             window['c'+which]()
             played.push(which)
         } else {
@@ -158,6 +159,168 @@ function c8() {
     playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
     playButton.style.backgroundColor = '#fff4f4';
 }
+
+function c9() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://playerservices.streamtheworld.com/api/livestream-redirect/KRXOFM_SC');
+    roundImg.setAttribute('src', 'channel_images/thefranchise.jpg');
+    playstr.innerHTML = "107.7 FM The Franchise";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c10() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://2.trafficstream.nl/allsports_main.mp3');
+    roundImg.setAttribute('src', 'channel_images/allsports.jpg');
+    playstr.innerHTML = "AllSportsRadio";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c11() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://playerservices.streamtheworld.com/api/livestream-redirect/KKSEFM_SC');
+    roundImg.setAttribute('src', 'channel_images/altitude.jpg');
+    playstr.innerHTML = "Altitude Sports 92.5 FM";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c12() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://stream.zeno.fm/wz94cu2m0rquv');
+    roundImg.setAttribute('src', 'channel_images/cwn.jpg');
+    playstr.innerHTML = "CWN Sports";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c13() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://s2.radio.co/s847c08ad4/listen');
+    roundImg.setAttribute('src', 'channel_images/fcum.jpg');
+    playstr.innerHTML = "FCUM Radio";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c14() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://playerservices.streamtheworld.com/api/livestream-redirect/KNBRAM_SC');
+    roundImg.setAttribute('src', 'channel_images/knbr.jpg');
+    playstr.innerHTML = "KNBR 104.5";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c15() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://playerservices.streamtheworld.com/api/livestream-redirect/KDZRAM_SC');
+    roundImg.setAttribute('src', 'channel_images/patriot.jpg');
+    playstr.innerHTML = "AM 1640 The Patriot";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c16() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://kcrw.streamguys1.com/kcrw_192k_mp3_news_internet_radio');
+    roundImg.setAttribute('src', 'channel_images/kcrw.jpg');
+    playstr.innerHTML = "KCRW 89.9 HD3 News 24";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c17() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://playerservices.streamtheworld.com/api/livestream-redirect/KDOWAM_SC');
+    roundImg.setAttribute('src', 'channel_images/kdow.jpg');
+    playstr.innerHTML = "KDOW 1220 AM";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c18() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://ice42.securenetsystems.net/KLOE');
+    roundImg.setAttribute('src', 'channel_images/kloe.jpg');
+    playstr.innerHTML = "KLOE 730 AM";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c19() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://live.wostreaming.net/direct/sinclair-komoammp3-ibc2');
+    roundImg.setAttribute('src', 'channel_images/komo.jpg');
+    playstr.innerHTML = "KOMO News 1000 AM";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c20() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://node-12.zeno.fm:80/8ty8szwpwfeuv');
+    roundImg.setAttribute('src', 'channel_images/newmix.png');
+    playstr.innerHTML = "Hits of Bollywood";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c21() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://listen.1brightonfm.co.uk:10000/obfm_mp3');
+    roundImg.setAttribute('src', 'channel_images/1btn.jpg');
+    playstr.innerHTML = "1BTN";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c22() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://bigrradio.cdnstream1.com/5124_128');
+    roundImg.setAttribute('src', 'channel_images/aceradio.jpg');
+    playstr.innerHTML = "AceRadio - Soft Hits";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c23() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://uk5.antennaradio.co.uk:8296/stream');
+    roundImg.setAttribute('src', 'channel_images/antenna.jpg');
+    playstr.innerHTML = "Antenna Radio";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+
+function c24() {
+    mytrack.pause();
+    mytrack.setAttribute('src', 'http://64.34.175.149:8000/blurfm03');
+    roundImg.setAttribute('src', 'channel_images/blurfm.jpg');
+    playstr.innerHTML = "Blur FM";
+    mytrack.play();
+    playButton.style.backgroundImage = 'url(icon/icons8-pause-50.png)';
+    playButton.style.backgroundColor = '#fff4f4';
+}
+	
+
 
 function playOrPause() {
     if (!mytrack.paused && !mytrack.ended) {
